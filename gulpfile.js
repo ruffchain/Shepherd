fa44de49fbf4028f98b0d4c88dab35c1da4733d9
yango3 <o3.yang@hotmail.com>
2018-11-29 15:36:06 +0800

    Wallet.sh to use wallet.js under dist directory

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ const gulp = require('gulp')
 const shell = require('gulp-shell')
 const clean = require('gulp-clean')
 const ts = require('gulp-typescript')
+const rename = require('gulp-rename');
+const flatten = require('gulp-flatten')
 const tsProject = ts.createProject('tsconfig.json')
 
 gulp.task('clean', () => {
-  return gulp
-    .src(['dist', 'data', 'build'], { read: false, allowEmpty: true })
-    .pipe(clean())
+    return gulp
+        .src(['dist', 'data', 'build'], {
+            read: false,
+            allowEmpty: true
+        })
+        .pipe(clean())
 })
 
 gulp.task('build-common', gulp.series(shell.task('cd common;npm run build')));
@@ -22,17 +27,27 @@ gulp.task('install-ruffvm-bindings', () => {
 });
 
 gulp.task('install-ruffvm', gulp.series([shell.task('cd ruffvm;npm install'),
-                                       'install-ruffvm-index',
-                                       'install-ruffvm-bindings']));
+    'install-ruffvm-index',
+    'install-ruffvm-bindings'
+]));
 
-gulp.task('compile', function() {
-  return tsProject
-    .src()
-    .pipe(tsProject())
-    .pipe(gulp.dest('dist/blockchain-sdk/ruffchain'))
+gulp.task('compile', function () {
+    return tsProject
+        .src()
+        .pipe(tsProject())
+        .pipe(flatten())
+        .pipe(gulp.dest('dist/blockchain-sdk/ruffchain'))
+})
+gulp.task('update-wallet', () => {
+    return gulp.src(['./dist/blockchain-sdk/ruffchain/wallet/chain/wallet.js'])
+        .pipe(gulp.dest('./common/dist/blockchain-sdk/demo/dpos/wallet'));
+})
+gulp.task('update-handler', () => {
+    return gulp.src(['./dist/blockchain-sdk/ruffchain/node/chain/handler.js'])
+        .pipe(gulp.dest('./common/dist/blockchain-sdk/demo/dpos/chain'));
 })
 
-gulp.task('build', gulp.series(['clean', 'build-common', 'install-ruffvm','compile'], () => {
+gulp.task('build', gulp.series(['clean', 'build-common', 'install-ruffvm', 'compile'], () => {
     return gulp.src(['programs/node/chain/*.json'])
         .pipe(gulp.dest('./dist/blockchain-sdk/ruffchain'));
 }));
