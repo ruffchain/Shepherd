ed7e30299c5835c04ffcbbc8ae65e47340d0801e
yango3 <o3.yang@hotmail.com>
2019-03-29 16:11:08 +0800

    add back transferbancortokento, getbancortokenbalance

---
+++ programs/wallet/src/lib/getBancorTokenBalance.ts
@@ -0,0 +1,74 @@
+import { ErrorCode } from "../core";
+import { IfResult, IfContext, checkTokenid, checkAddress, formatNumber } from './common';
+
+const METHOD_NAME = 'view';
+const FUNC_NAME = 'getBancorTokenBalance';
+
+export async function getBancorTokenBalance(ctx: IfContext, args: string[]): Promise<IfResult> {
+    return new Promise<IfResult>(async (resolve) => {
+
+        // check args
+        if (args.length < 2) {
+            resolve({
+                ret: ErrorCode.RESULT_WRONG_ARG,
+                resp: "Wrong args"
+            });
+            return;
+        }
+
+        if (!checkTokenid(args[0])) {
+            resolve({
+                ret: ErrorCode.RESULT_WRONG_ARG,
+                resp: "Wrong tokenid , length [3-12]"
+            });
+            return;
+        }
+        if (!checkAddress(args[1])) {
+            resolve({
+                ret: ErrorCode.RESULT_WRONG_ARG,
+                resp: "Wrong address "
+            });
+            return;
+        }
+
+        let params =
+        {
+            method: 'getBancorTokenBalance',
+            params: {
+                tokenid: args[0],
+                address: args[1]
+            }
+        }
+
+        let cr = await ctx.client.callAsync(METHOD_NAME, params);
+        if (ctx.sysinfo.verbose) {
+            console.log(cr);
+        }
+
+        resolve(cr);
+    });
+}
+export function prnGetBancorTokenBalance(ctx: IfContext, obj: IfResult) {
+    if (ctx.sysinfo.verbose) {
+        console.log(obj);
+    }
+
+    console.log('');
+
+    if (!obj.resp) {
+        console.log('Wrong result: ');
+        return;
+    }
+    let objJson: any;
+    try {
+        objJson = JSON.parse(obj.resp);
+        if (objJson.err === 0) {
+            console.log('Balance: ', formatNumber(objJson.value));
+        } else {
+            console.log('Error:', objJson.err);
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+}
